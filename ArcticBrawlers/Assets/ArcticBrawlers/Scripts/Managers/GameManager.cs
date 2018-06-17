dd2171b394af28891fad4e2b250299728dfd06b8
Scott - Laptop <scott.melanson3@gmail.com>
2018-06-17 16:24:31 -0400

    Working on multiplayer support

---
+++ ArcticBrawlers/Assets/ArcticBrawlers/Scripts/Managers/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager Instance { get; private set; }
+
+    private UserInputObject[] playerInputs;
+
+    // Called on object creation
+    void Awake()
+    {
+        // Singleton pattern
+        if (Instance == null)
+        {
+            Instance = this;
+        } else
+        {
+            Debug.Log("Warning: multiple " + this + " in scene!");
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ ArcticBrawlers/Assets/ArcticBrawlers/Scripts/ScriptableObjects/UserInputObject.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "User Input", menuName = "Data/Input", order = 1)]
+public class UserInputObject : ScriptableObject {
+
+    [Header("Basic Info")]
+    public int playerID;
+
+    [Header("Input Manager Values")]
+
+    public string horizontalMovement;
+    public string verticalMovement;
+    public string jumpButton;
+    public string throwSnowball;
+    public string reloadSnowball;
+    public string pauseButton;
+}
